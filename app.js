a3ae9fe9dc2fc0d9cc790f0f73db2b7e8fc47d43
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-07-23 00:41:13 +0700

    d

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const router = require('./routes');
-const log = require('./middlewares/logger');
 
-app.use(log);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use(router);
-app.use((req, res, next) => {
-    res.status(404)
-    res.send({
-        status: 'failed',
-        message: 'rsource' + req.originalUrl + 'not found'
-    })
-})
 app.listen(5000, () => console.log('server: http://localhost:5000'))
--- routes.js
+++ routes.js
@@ -1,8 +1,4 @@
 const router = require('express').Router();
-const multer = require('multer');
-const upload = multer({ dest: 'uploads' });
-const fs = require('fs');
-const path = require('path');
 
 router.get('/', (req, res) => {
     const { page, total } = req.query
@@ -12,37 +8,6 @@ router.get('/', (req, res) => {
         study: 'express js',
         page,
         total
-    });
-});
-
-router.get('/product/:id', (req, res) => {
-    res.send({
-        id: req.params.id
-    });
-});
-router.post('/product/', upload.single('image'), (req, res) => {
-    const { name, price, stock, status } = req.body;
-    // console.log(req.file)
-    const image = req.file;
-    if (image) {
-        const target = path.join(__dirname, 'uploads', image.originalname);
-        fs.renameSync(image.path, target)
-        // res.json({
-        //     name,
-        //     price,
-        //     stock,
-        //     status,
-        //     image
-        // });
-        res.sendFile(target);
-    }
-});
-
-router.get('/:category/:tag', (req, res) => {
-    const { category, tag } = req.params
-    res.send({
-        category: category,
-        tag: tag
     });
 });
 
