e3d5ad58116ec2f93fc4dce8ff9ded25c7ffb8e9
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-07-23 16:27:00 +0700

    h

--- routes.js
+++ routes.js
@@ -17,4 +17,20 @@ router.get('/product/:id', (req, res) => {
     });
 });
 
+router.get('/:category/:tag', (req, res) => {
+    const { category, tag } = req.params
+    res.send({
+        category: category,
+        tag: tag
+    });
+});
+
+router.get('/contact', (req, res) => {
+    res.send({
+        nama: "Zawait",
+        email: "zaw@gmail.com",
+        noHp: "0987654321"
+    });
+});
+
 module.exports = router;
