eeb20301fa396de6d44454d35bbf5de6e0895533
DESKTOP-H2UVHVF\PSK GAMING <ainuttaqimzawaid1000@gmail.com>
2023-07-23 16:34:49 +0700

    f

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ const app = express();
 const router = require('./routes');
 
 app.use(router);
+app.use((req, res, next) => {
+    res.status(404)
+    res.send({
+        status: 'failed',
+        message: 'rsource' + req.originalUrl + 'not found'
+    })
+})
 app.listen(5000, () => console.log('server: http://localhost:5000'))
